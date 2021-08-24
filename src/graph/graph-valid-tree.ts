44055f3414eecd2fc9cd3af00b6863d8469c5f13
John Peebles <jpeeblesg@gmail.com>
2021-08-23 18:41:51 -0700

    Graph valid tree

---
+++ src/graph/graph-valid-tree.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/graph-valid-tree/
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {boolean}
+ */
+// Input: n = 5, edges = [[0, 1], [0, 2], [0, 3], [1, 4]]
+// Output: true
+
+const validTree = function (n, edges) {
+
+};
